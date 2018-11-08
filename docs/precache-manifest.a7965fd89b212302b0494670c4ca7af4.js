256aada3dc667610cbe4a740681e5ae694cbf171
Agnes <agnes@MacBook-Pro-65.local>
2018-11-08 17:06:49 +0100

    Ad first version of calender

---
+++ docs/precache-manifest.a7965fd89b212302b0494670c4ca7af4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3440d942dd2d2b619044",
+    "url": "/static/js/main.3440d942.chunk.js"
+  },
+  {
+    "revision": "f4926859e068d12e2815",
+    "url": "/static/js/1.f4926859.chunk.js"
+  },
+  {
+    "revision": "3440d942dd2d2b619044",
+    "url": "/static/css/main.9624e045.chunk.css"
+  },
+  {
+    "revision": "da54da18a0a95547b978bffe6c140cc5",
+    "url": "/index.html"
+  }
+];
